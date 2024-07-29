50004dbbababde84d183ccd9b8d04d53011d11fc
aabunani <anselmobunani@gmail.com>
2024-07-30 07:15:36 +0800

    fix: remove onchange

--- grove_purchase_enhancement/models/stock_move.py
+++ grove_purchase_enhancement/models/stock_move.py
@@ -10,13 +10,6 @@ class StockMove(models.Model):
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    api.onchange('product_tmpl_id')
-    def _onchange_product_id(self):
-        if self.product_tmpl_id:
-            self.sku = self.product_tmpl_id.sku
-        else:
-            self.sku = ""
-
     sku = fields.Char(string='SKU')
 
 class ProductTemplate(models.Model):
